aafa67c68a4fe5ab4a7af6de0fa9b58c4ce8af38
Will Yip <will@cloudful.io>
2025-10-25 22:34:15 -0400

    Updated formatTimeAgo function to ignore anything over 1 month

--- src/lib/formatTimeAgo.ts
+++ src/lib/formatTimeAgo.ts
@@ -43,7 +43,7 @@ export function formatTimeAgo(
     // Example: "Oct 25, 2025"
     return date.toLocaleDateString(locale, {
       year: "numeric",
-      month: "short",
+      month: "long",
       day: "numeric",
     });
   }
